344e55f36668305ec57e574ed008ae6d70638646
someonempty <someonempty@gmail.com>
2023-03-23 23:53:02 +0300

    Добавляет задачу по нажатию на enter

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import { useState } from "react";
 import List from "./components/List";
 import Task from "./components/List/Task";
 
+// const addTask = () => {
+	// 	setTasks([...tasks, {}])
+	// }
+
 function App() {
-	const [tasks, setTasks] = useState();
+	const [tasks, setTasks] = useState('');
 
 	const addTask = () => {
 		if (tasks) {
-			return <Task text={tasks}/>
-		} else {
-			console.log('bober')
+			return <Task text={tasks.text}/>
 		}
-		
 	}
+
 	return (
 		<>
 		<List tasks={tasks} setTasks={setTasks} addTask={addTask}/>
 		</>
 	);
 }
+
 
 export default App;
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -2,29 +2,40 @@ import React, { useState } from 'react';
 import Task from './Task';
 import './index.css';
 
-const List = ({ tasks, setTasks, addTask}) => {
+const List = ({ tasks, setTasks, addTask }) => {
 	const [value, setValue] = useState('');
 
-	console.log(tasks)
-	const TaskRender = () => {
-		console.log('rendering task')
-	}
 	return (
 		<div className='container'>
 			<h1 className='todolist-header'>Inbox</h1>
 			<div className='wrapper'>
-				<input value={value} placeholder='New task' onChange={(e) => setValue(e.target.value)} type='text' className='task-input' />
+				<input
+					value={value}
+					placeholder='New task'
+					onKeyDown={(e) => {
+						if (e.keyCode === 13) {
+							setTasks({ text: value });
+							setValue('');
+							console.log(tasks);
+						}
+					}}
+					onChange={(e) => {
+						setValue(e.target.value);
+					}}
+					type='text'
+					className='task-input'
+				/>
 				<button
-				 onClick={() => {
-					setTasks(value)
-					TaskRender()
-				 } } className='adding-button'>
+					onClick={() => {
+						setTasks({ text: value });
+						setValue('');
+					}}
+					className='adding-button'
+				>
 					Add
 				</button>
 			</div>
-			<div className='tasks-list'>
-				{addTask()}
-			</div>
+			<div className='tasks-list'>{addTask()}</div>
 		</div>
 	);
 };
